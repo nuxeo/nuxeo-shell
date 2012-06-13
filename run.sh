bb1525cbb91caabebf1551e93add22c8ce2c1bd8
Jenkins Nuxeo <jenkins@nuxeo.com>
2012-06-13 09:57:15 +0200

    Release 5.4.2-HF24

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 
 #JAVA_OPTS="-Xdebug -Xrunjdwp:transport=dt_socket,address=8000,server=y,suspend=n"
 
-java ${JAVA_OPTS} -cp nuxeo-shell-distribution/target/nuxeo-shell-5.4.2.1-SNAPSHOT.jar org.nuxeo.shell.Main http://localhost:8080/nuxeo/site/automation
+java ${JAVA_OPTS} -cp nuxeo-shell-distribution/target/nuxeo-shell-5.4.2-HF24.jar org.nuxeo.shell.Main http://localhost:8080/nuxeo/site/automation
