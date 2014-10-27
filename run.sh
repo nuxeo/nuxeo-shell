c5b922a5c7f11fe9627b17334163a8457fb1afa4
Jenkins Nuxeo <jenkins@nuxeo.com>
2014-10-27 20:21:34 +0100

    Release master, update 5.9.6-SNAPSHOT to 5.9.6-I20141027_2013

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 
 JAVA_OPTS="-Xdebug -Xrunjdwp:transport=dt_socket,address=8000,server=y,suspend=n"
 
-java ${JAVA_OPTS} -cp nuxeo-shell-distribution/target/nuxeo-shell-5.9.6-SNAPSHOT.jar org.nuxeo.shell.Main http://localhost:8080/nuxeo/site/automation
+java ${JAVA_OPTS} -cp nuxeo-shell-distribution/target/nuxeo-shell-5.9.6-I20141027_2013.jar org.nuxeo.shell.Main http://localhost:8080/nuxeo/site/automation
