2e9e43bfe9595e2b3bcfc3a02a54e3e30147daeb
Jenkins Nuxeo <jenkins@nuxeo.com>
2016-01-28 09:53:07 +0000

    Release 6.0, update 6.0-HF26-SNAPSHOT to 6.0-HF26

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 
 JAVA_OPTS="-Xdebug -Xrunjdwp:transport=dt_socket,address=8000,server=y,suspend=n"
 
-java ${JAVA_OPTS} -cp nuxeo-shell-distribution/target/nuxeo-shell-6.0-HF26-SNAPSHOT.jar org.nuxeo.shell.Main http://localhost:8080/nuxeo/site/automation
+java ${JAVA_OPTS} -cp nuxeo-shell-distribution/target/nuxeo-shell-6.0-HF26.jar org.nuxeo.shell.Main http://localhost:8080/nuxeo/site/automation
