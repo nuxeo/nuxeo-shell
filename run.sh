abf4f393b38fd9047c90ef0341ad9d6491a4d010
Jenkins Nuxeo <jenkins@nuxeo.com>
2018-09-21 05:21:16 +0000

    Post release 9.10-HF18 Update 9.10-HF18-SNAPSHOT to 9.10-HF19-SNAPSHOT

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 
 JAVA_OPTS="-Xdebug -Xrunjdwp:transport=dt_socket,address=8000,server=y,suspend=n"
 
-java ${JAVA_OPTS} -cp nuxeo-shell-distribution/target/nuxeo-shell-9.10-HF18-SNAPSHOT.jar org.nuxeo.shell.Main http://localhost:8080/nuxeo/site/automation
+java ${JAVA_OPTS} -cp nuxeo-shell-distribution/target/nuxeo-shell-9.10-HF19-SNAPSHOT.jar org.nuxeo.shell.Main http://localhost:8080/nuxeo/site/automation
