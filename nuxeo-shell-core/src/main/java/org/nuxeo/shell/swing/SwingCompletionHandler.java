8baeef3311f91cab6ebe50384cbea2acb5ed9ce0
Pierre GAUTIER <pierregautier@nuxeo.com>
2019-02-14 12:37:39 +0100

    NXP-26194: Remove usage of StringBuffer

--- nuxeo-shell-core/src/main/java/org/nuxeo/shell/swing/SwingCompletionHandler.java
+++ nuxeo-shell-core/src/main/java/org/nuxeo/shell/swing/SwingCompletionHandler.java
@@ -94,7 +94,7 @@ public class SwingCompletionHandler implements CompletionHandler {
         String[] strings = (String[]) candidates.toArray(new String[candidates.size()]);
 
         String first = strings[0];
-        StringBuffer candidate = new StringBuffer();
+        StringBuilder candidate = new StringBuilder();
 
         for (int i = 0; i < first.length(); i++) {
             if (startsWith(first.substring(0, i + 1), strings)) {
