64c0fa0c00ae33c43a2bc48c62a67ca2b3007a61
Jenkins Nuxeo <jenkins@nuxeo.com>
2019-08-06 10:50:37 +0000

    Post release 10.10-HF11 Update 10.10-HF11-SNAPSHOT to 10.10-HF12-SNAPSHOT

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 
 JAVA_OPTS="-Xdebug -Xrunjdwp:transport=dt_socket,address=8000,server=y,suspend=n"
 
-java ${JAVA_OPTS} -cp nuxeo-shell-distribution/target/nuxeo-shell-10.10-HF11-SNAPSHOT.jar org.nuxeo.shell.Main http://localhost:8080/nuxeo/site/automation
+java ${JAVA_OPTS} -cp nuxeo-shell-distribution/target/nuxeo-shell-10.10-HF12-SNAPSHOT.jar org.nuxeo.shell.Main http://localhost:8080/nuxeo/site/automation
